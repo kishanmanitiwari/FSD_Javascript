e71c8482d6295fca191619d50d3467b31625870a
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-06-16 12:10:28 +0530

    Day-2 Code

---
+++ Day_2/javacripts.js
@@ -0,0 +1,120 @@
+//Print code
+// console.log('Hello JS')
+
+//Variables
+
+// var age = 24
+// var price = 100
+// var cgpa = 9.55
+// var isTrue = false
+
+//fullName = 'Rahul Sharma' //updated
+
+// var fullName = 'kishan tiwari'
+// var fullName = 'Virat kohli'
+// var fullName = 'Rohit Sharma'
+
+
+
+// let fullName = 'Virat'
+// fullName = 'Virat kohli'
+
+// let fullName = 'rohit'
+
+// console.log(fullName);
+
+
+// {
+// //Block -1
+// var school = 'DYP'
+
+// // }
+// let school = 'DYP'
+// console.log(school);
+
+// const pi = 3.14
+// //update
+// pi = 6.86
+
+// const pi = 34
+
+// myAge = 66
+
+// console.log(pi);
+
+//Data-Type
+
+// let age = 52
+
+// let myName = 'Rohan'
+
+// let isFollow = false
+
+// let cgpa;
+
+// let obj = null;
+
+// let bigNumber = BigInt(122873783)
+
+// let symbol = Symbol("Hello!")
+
+
+// console.log(typeof age);
+
+// console.log(typeof myName);
+
+// console.log(typeof isFollow);
+
+// console.log(typeof cgpa);
+
+// console.log(typeof obj);
+
+// console.log(typeof bigNumber);
+
+// console.log(typeof symbol);
+
+
+
+//Objects in JS
+
+const student1 = {
+    myName:'Rahul',
+    age:23,
+    marks:74,
+    isPass:null,
+    college:'IIT Bombay'
+}
+
+// console.log(typeof student1);
+
+
+// console.log(student1);
+
+// console.log(typeof student1);
+
+// console.log(student1['isPass']);
+
+// console.log(student1.myName);
+// console.log(student1.college);
+
+// const user = {
+//     userName : 'Akshat Jain', //string
+//     Role: 'SWE', //string
+//     Company: 'Microsoft', //string
+//     Loaction: 'Bangaluru', //string
+//     Connection: 500,//number
+//     ifFollow: true//boolean
+// }
+
+// console.log(user);
+
+const product = {
+    productName : 'Ball Pen',
+    color : 'Black',
+    Rating : 4.5,
+    isDeal:true,
+    price:270,
+    discount:5
+}
+
+console.log(product);
