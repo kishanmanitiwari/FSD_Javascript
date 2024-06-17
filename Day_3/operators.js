e2bc552f94c7275bd295c20e6b370fe9d3dcd05b
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-06-17 11:46:13 +0530

    Day-3_Operators and Comments

---
+++ Day_3/operators.js
@@ -0,0 +1,91 @@
+//This is a comment
+
+
+//Operators in JS ->
+
+//1) Arithmetic Operators
+
+let a = 5
+let b = 2
+
+console.log('a =',a ,"-" ,'b =',b);
+
+//Additional operator
+
+
+// console.log('The additon is',a+b)
+// console.log('The subtraction is',a-b)
+// console.log('The Multiplication is',a*b)
+// console.log('The division is',a/b)
+// console.log('The modulus is',a%b)
+// console.log('The expontient is',b**a);
+
+// a++; // a= a+1
+// b--; // b = b-1
+
+// console.log('The post-increment is',a)
+// console.log('The post decrement is',b);
+
+
+// console.log('The post increment is' , a++);
+
+// console.log('The pr-incement is', ++a);
+// console.log('The post-decrement for b is', b--);
+// console.log(b);
+
+//Assignmnet - Operators
+
+// let c = a+=b; // a+b (5+2)
+// console.log(c);
+
+// let d= a-=b; // a-b
+// console.log(d);
+
+// let e= a*=b; //a*b
+// console.log(e);
+
+// let f = a%=b; //a%b
+// console.log(f);
+
+// let g = a**=b //a^b
+// console.log(g);
+
+
+
+//Comparsion - Operator
+
+//let c='2'
+
+// console.log('Equal=>',b==a);
+// console.log('NOT Equal=>',a!=b);
+
+// console.log('Greater',a>b);
+// console.log('Greater Eqaul',5>=5);
+
+// console.log('isEqual',(a===c));
+
+
+// console.log('isEqual',(a!==c));
+
+
+
+// let condition1 = (a===b); //false
+// let condition2 = (6===6);//true
+
+// console.log('AND Operator',condition1 && condition2);
+// console.log('OR Operator',condition1 || condition2);
+
+ 
+// console.log('NOT',(!condition2));//true
+
+//Doubt by Prashant->
+
+// let s = Symbol('hello');
+// let d = s;
+
+// console.log('Comparing Symbol',(s===d));
+
+
+
+
+
