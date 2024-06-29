62701994a1aa6758d8712a19be072c0c52d36e34
Kishan Tiwari <kishanmanitiwari@gmail.com>
2024-06-29 11:53:00 +0530

    Day_13_DOM_Part_2

---
+++ Day_13_DOM_2/index.js
@@ -0,0 +1,21 @@
+
+let h1 = document.querySelector('h1');
+
+//console.log(h1.getAttributeNames());
+
+// console.log(h1.getAttribute('id'));
+
+// console.log(h1.getAttribute('class'));
+
+// h1.setAttribute('class','headings');
+
+// console.log(h1.getAttribute('class'));
+
+
+
+ h1.classList.add('white');
+
+
+
+
+
